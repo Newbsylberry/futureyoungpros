ae9d0be1f2efe7cc7cd3f543e09fd6ac83ecc3c3
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-10-13 14:54:41 -0400

    Completed Basic authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   def after_sign_in_path_for(resource)
-    welcome_professional_path
+    @user = current_user
+    if @user.role == 'professional'
+      welcome_professional_path
+    elsif @user.role == 'admin'
+      welcome_admin_path
+    end
   end
 
 
@@ -15,6 +20,9 @@ class ApplicationController < ActionController::Base
   
  def configure_permitted_parameters
   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:role, :email, :password ) }
+   
+   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+
  end
 
 
