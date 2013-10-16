eb1300f81095f6044f0ae5fb60d5662b30299ce5
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-10-16 01:56:44 -0400

    Added images and staticbanner layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,26 @@
 class StaticPagesController < ApplicationController
   def home
+    
   end
 
   def students
+    @pagename = "students"
   end
 
   def professionals
+    @pagename = "professionals"
   end
 
   def administrators
+    @pagename = "administrators"
   end
 
   def businesses
+    @pagename = "businesses"
   end
 
   def schools
+    @pagename = "schools"
   end
 
 end
