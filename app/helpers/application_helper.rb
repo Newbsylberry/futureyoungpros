2069b46b514bf99855fa2060d9947a6c7901bbe4
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-10-22 12:38:22 -0400

    Fixed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 def full_title(page_title)
-  base_title = "Future Young Professionals | "
+  base_title = "Future Young Professionals"
     if page_title.empty?
       base_title
     else
