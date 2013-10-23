d7528f462cf46d294eae80557374b33a718b03eb
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-10-22 22:05:30 -0400

    made user model secure

--- db/migrate/20131023015157_add_category_idto_mous.rb
+++
@@ -1,7 +0,0 @@
-class AddCategoryIdtoMous < ActiveRecord::Migration
-  def change
-    add_column :mous, :category_id, :integer
-    add_column :mous, :subcategory_id, :integer
-    add_column :mous, :business_id, :integer
-  end
-end
