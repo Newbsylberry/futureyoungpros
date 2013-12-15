95d9790540458c35ebb5d1c816914cc5e7f6bd3c
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-12-15 12:08:55 -0500

    Fixed multiple recipient issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "admin@future40b.com"
-
+  default to: Proc.new { 'chris.s.mccarthy@gmail.com', 'tpalermo@saltcitydistrict.com' },
+          from: "admin@future40b.com"
+  
   def registration_email(mou)
     @mou = mou
-    mail(to: @mou.emailAddress, 'chris.s.mccarthy@gmail.com', 'tpalermo@saltcitydistrict.com' 
+    mail(to: @mou.emailAddress, 
          subject: "Thanks for Registering with the Future40B Program!")
-  end
-    
-
+  end  
 end
